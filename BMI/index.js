64b3bdfc4d53a82a8fb4c768701204faf4bf4498
QuangTD <quangtd_tts@rikkeisoft.com>
2024-12-05 08:38:10 +0700

    complete BMI

--- BMI/index.js
+++ BMI/index.js
@@ -1,6 +1,9 @@
 function calculateBMI() {
   const resultElement = document.getElementById('result');
   //TODO
+  let height = document.getElementById('height').value;
+  let weight = document.getElementById('weight').value;
 
-  resultElement.textContent = `Your BMI is .`;
+  let result = weight / (Math.pow(height, 2));
+  resultElement.innerHTML = 'Your BMI is '+result;
 }
