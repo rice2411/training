c8ea7bc732dcf64c501fd23ed4b48661542dda71
QuangTD <quangtd_tts@rikkeisoft.com>
2024-12-05 10:45:09 +0700

    Fix BMI

--- BMI/index.js
+++ BMI/index.js
@@ -5,5 +5,16 @@ function calculateBMI() {
   let weight = document.getElementById('weight').value;
 
   let result = weight / (Math.pow(height, 2));
-  resultElement.innerHTML = 'Your BMI is '+result;
+
+  if (height == '' || weight == '' || height >= 200 || weight >= 200) {
+    alert('Hãy nhập giá trị hợp lệ')
+  } else if (result < 18.5) {
+    resultElement.innerHTML = 'Your BMI is '+result.toFixed(1) + ' (Underweight)';
+  }else if (result >= 18.5 && result < 25) {
+    resultElement.innerHTML = 'Your BMI is '+result.toFixed(1) + ' (Normal)';
+  }else if (result >= 25 && result < 30) {
+    resultElement.innerHTML = 'Your BMI is '+result.toFixed(1) + ' (Overwweight)';
+  }else {
+    resultElement.innerHTML = 'Your BMI is '+result.toFixed(1) + ' (Obese)';
+  }
 }
