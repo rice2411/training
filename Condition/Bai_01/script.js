a9d8697a9e855935007fd0e56d04980f9cfeaf9e
QuangTD <quangtd_tts@rikkeisoft.com>
2024-12-06 10:18:01 +0700

    Fix bai 1

--- Condition/Bai_01/script.js
+++ Condition/Bai_01/script.js
@@ -2,21 +2,11 @@ function check() {
     const a = document.getElementById('a').value;
     const b = document.getElementById('b').value;
 
-    // var result = document.getElementById('calResult');
-    var oldResult = document.getElementById('calResult');
-    if (oldResult) {
-        console.log(oldResult);
-        oldResult.remove();
-    }
+    var result = document.getElementById('calResult');
 
-    var result = document.createElement('div');
-    result.id = 'calResult';
-    result.style.marginTop = '20px';
-    result.style.fontSize = '15px';
-    result.style.color = '#333';
-    document.getElementById('divContainer').appendChild(result);
-
-    if (a%b == 0) {
+    if(a == '' || b == '') {
+        alert('Hãy nhập giá trị hợp lệ')
+    }else if (a%b == 0) {
         result.innerHTML = 'a chia hết cho b';
     }else{
         result.innerHTML = 'a không chia hết cho b';
